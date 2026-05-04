e2ef68c39ea2eb536ec6eeaf86a1eff36cf7b9ce
rrene7 <rrenys.evang0907@gmail.com>
2026-05-04 10:48:48 -0500

    Add PHP API base config

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+// Configuracion base API
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+require_once __DIR__ . '/db.php';
