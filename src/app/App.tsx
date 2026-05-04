6fa9d212325d13359d732cab2d15bb676c1e600a
rrene7 <rrenys.evang0907@gmail.com>
2026-05-04 16:41:48 -0500

    Register salon registration route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { SalonDetails } from "@/app/components/SalonDetails";
 import { AdminPanel } from "@/app/components/AdminPanel";
 import { MisCitas } from "@/app/components/MisCitas";
 import Login from "@/app/pages/Login";
+import InscribirSalon from "@/app/pages/InscribirSalon";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
           <Route path="/mis-citas" element={<MisCitas />} />
           <Route path="/admin" element={<AdminPanel />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/inscribir-salon" element={<InscribirSalon />} />
         </Routes>
         <Toaster />
       </div>
